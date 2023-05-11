09155828a6a22962da298fdf2a56ef04e3b43ee7
Klariff <jpvera.f@gmail.com>
2023-05-10 19:57:34 -0500

    CORS

--- src/api/main.py
+++ src/api/main.py
@@ -3,7 +3,19 @@ from fastapi.responses import RedirectResponse
 from nlp.embeddings import embed
 import requests as r
 from enum import Enum
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
  
 class Models(Enum):
